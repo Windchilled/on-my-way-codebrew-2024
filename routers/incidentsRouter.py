6e5c9f6e648d728221e6c5fce9ce673558e8b31f
Windchilled <lhastings@student.unimelb.edu.au>
2024-04-05 12:50:55 +1100

    can send json data to the router now

--- routers/incidentsRouter.py
+++ routers/incidentsRouter.py
@@ -1,11 +1,16 @@
 from flask import Flask,request,jsonify
 from flask import Blueprint
-from controllers import test
+#from controllers import test
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from controllers.incidentController import *
 
 incident = Blueprint("incident",__name__)
 
 @incident.route('/add', methods = ['POST','GET'])
 def addIncident():
-    data = request.json
-    result = test.test(data)
+    json_data = request.json
+    #result = test.test(data)
+    result = runAddIncident(json_data)
     return jsonify(result)
