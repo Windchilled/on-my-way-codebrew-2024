f547a41d21c30e1ec1942f274def951ab0c42e1d
Windchilled <lhastings@student.unimelb.edu.au>
2024-04-05 17:27:41 +1100

    removed code creating new incident posts every time program is run

--- database.py
+++ database.py
@@ -8,17 +8,3 @@ uri = "mongodb+srv://ashhastings:Qq3ydvtj67ywtDZI@cluster0.acct40j.mongodb.net/?
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['OnMyWay']
 collection = db['Incidents']
-
-document = {"name":"ash","city":"melbourne"}
-insert_doc = collection.insert_one(document)
-
-print(f"inserted doc ID: {insert_doc.inserted_id}")
-client.close()
-
-
-''' Send a ping to confirm a successful connection
-#try:
-#    client.admin.command('ping')
-#    print("Pinged your deployment. You successfully connected to MongoDB!")
-#except Exception as e:
-#    print(e)'''
