02fdb0935ab0c0d032dfc95e4f6c2b2babec72a0
maheen562 <maheenkhan193@gmail.com>
2024-04-05 22:55:35 +1100

    "added footer.component"

--- on-my-way-frontend/src/app/componenets/footer/footer.component.ts
+++ on-my-way-frontend/src/app/componenets/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   standalone: true,
   imports: [],
+
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
