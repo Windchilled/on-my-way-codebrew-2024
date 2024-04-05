70c72a995de3b7b1c62bd9986a98c64e2d8e6cbd
102560056u <102560056@student.swin.edu.au>
2024-04-05 15:47:17 +1100

    end variables fixed

--- on-my-way-frontend/src/app/componenets/map/map.component.ts
+++ on-my-way-frontend/src/app/componenets/map/map.component.ts
@@ -20,7 +20,8 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
      this.map = new mapboxgl.Map({
-        accessToken: environment.mapbox.accessToken,
+        // accessToken: environment.mapbox.accessToken,
+        accessToken: 'pk.eyJ1IjoiMTAyNTYwMDU2IiwiYSI6ImNsdWtwYzB6bDBxcW4yaWsyYXFnY3d0MjIifQ.KMav3MKT5-f5hSZoKPliFQ',
         container: 'map',
         style: this.style,
         zoom: 2,
